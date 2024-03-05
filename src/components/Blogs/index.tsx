91f10274780440a6aa1d1119fbe2704215df0429
Aditya Prasad Mohanty <adityaprasadmohanty150@gmail.com>
2024-03-06 00:10:28 +0530

    file changes

--- src/components/Blogs/index.tsx
+++ src/components/Blogs/index.tsx
@@ -2,46 +2,46 @@ import React, { useState, useRef } from "react";
 import { Link, graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-const BlogCarousel: React.FC<Queries.BlogCarouselComponent> = ({data}) => {
-  const [dragging, setDragging] = useState(false);
-  const [startX, setStartX] = useState(0);
-  const [scrollLeft, setScrollLeft] = useState(0);
-  return (
-    <>
-      <ul>
-          {data.allMdx.edges.map(({ node }) => (
-            <li key={node.id} className="mb-4 last-of-type:mb-0">
-              <Link
-                to={`/blog/${node.frontmatter?.slug}`}
-                className="block rounded-lg border border-gray-400 p-6"
-              >
-                <div className="flex-1 md:h-72 md:w-full">
-                  {node.frontmatter?.featuredImage && (
-                    <GatsbyImage
-                      image={
-                        getImage(
-                          node.frontmatter.featuredImage.childImageSharp,
-                        )!
-                      }
-                      alt=""
-                      className="h-full w-full"
-                    />
-                  )}
-                </div>
-                <h2 className="mb-4 text-xl font-bold">
-                  {node.frontmatter?.title}
-                </h2>
-                <span className="mb-2 block text-sm font-thin">
-                  By {node.frontmatter?.author} on {node.frontmatter?.date}
-                </span>
-                <span className="block text-lg">{node.excerpt}</span>
-              </Link>
-            </li>
-          ))}
-      </ul>
-    </>
-  );
-}
+// const BlogCarousel: React.FC<Queries.BlogCarouselComponent> = ({data}) => {
+//   const [dragging, setDragging] = useState(false);
+//   const [startX, setStartX] = useState(0);
+//   const [scrollLeft, setScrollLeft] = useState(0);
+//   return (
+//     <>
+//       <ul>
+//           {data.allMdx.edges.map(({ node }) => (
+//             <li key={node.id} className="mb-4 last-of-type:mb-0">
+//               <Link
+//                 to={`/blog/${node.frontmatter?.slug}`}
+//                 className="block rounded-lg border border-gray-400 p-6"
+//               >
+//                 <div className="flex-1 md:h-72 md:w-full">
+//                   {node.frontmatter?.featuredImage && (
+//                     <GatsbyImage
+//                       image={
+//                         getImage(
+//                           node.frontmatter.featuredImage.childImageSharp,
+//                         )!
+//                       }
+//                       alt=""
+//                       className="h-full w-full"
+//                     />
+//                   )}
+//                 </div>
+//                 <h2 className="mb-4 text-xl font-bold">
+//                   {node.frontmatter?.title}
+//                 </h2>
+//                 <span className="mb-2 block text-sm font-thin">
+//                   By {node.frontmatter?.author} on {node.frontmatter?.date}
+//                 </span>
+//                 <span className="block text-lg">{node.excerpt}</span>
+//               </Link>
+//             </li>
+//           ))}
+//       </ul>
+//     </>
+//   );
+// }
 
 export default function Blogs() {
   return (
@@ -53,7 +53,7 @@ export default function Blogs() {
       <h1 className="font-serif text-[3.25rem] text-white w-2/5 pl-[6.25rem]">
         Check out our blogs to maximize your growth
       </h1>
-      <BlogCarousel/>
+      {/* <BlogCarousel/> */}
     </section>
   );
 }
